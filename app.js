7edcf1ead9506306c10c169532030052e489c0e0
Abdul-Rafayy <155812890+Abdul-Rafayy@users.noreply.github.com>
2024-03-06 12:37:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+// Chapter 01
+// Q 01
+// alert("Howdy!")
+// Q 02
+// alert("Error! Please enter a valid password.")
+// Q 03
+// alert("Welcome to JS land...\nHappy coding!")
+// Q 04
+// alert("Welcome to JS land...")
+// alert("Happy coding!\nPrevent this page from creating additional dialogs.")
+// Q 05 
+// console.log("Hello... i can run JS through my web browser's console")
+// Q 06
+// Done!
+// Q 07
+// Done!
+// Chapter 02
+// Q 01
+// var username = "Rafay"
+// alert(username)
+// Q 02
+// var myName = "Abdul Rafay"
+// alert(myName)
+// Q 03
+// var message= "Hello World"
+// alert(message)
+// Q 04
+// var names = "John Doe"
+// alert(names)
+// var age = "15 years old"
+// alert(age)
+// var msg = "Certified Mobile Application Development"
+// alert(msg)
+// Q 05
+// var pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+// Q 06
+// var email = "My email address is abdul765rafay@gmail.com"
+// alert(email)
+// Q 07
+// var book = "I am trying to learn from the Book A smarter way to learn JavaScript"
+// alert(book)
+// Q 08
+// document.write("Yah! I can write HTML content through JavaScript")
+// Q 09
+// var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(symbol)
+// Chapter 03
+// Q 01
+// var old = "I am 15 years old"
+// alert(old)
+// Q 02
+// var visit = "You have visited this site 14 times"
+// alert(visit)
+// Q 03
+// var birthYear = 1990;
+// document.write("My birth year is " + birthYear + "<br> Data type of my declared variable is number");
+// Q 04
+// var nam = "John Doe"
+// var t ="T-shirt(s)"
+// var qty ="5 "
+// document.write("<b>"+nam+"</b>"+"\n ordered \n"+"<b>"+qty+"</b>"+"<b>"+t+"</b>"+"\non XYZ Clothing store")
